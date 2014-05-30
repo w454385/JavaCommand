36229a77442b48dde07b96e14130ce058d84cb4b
w454385 <w454385@fastmail.fm>
2014-05-30 15:44:09 +0200

    Coś takiego

--- src/com/company/King.java
+++ src/com/company/King.java
@@ -16,5 +16,9 @@ public class King {
     }
 
 	public String toString() { return "King " + name; }
+        
+        String getSomething() {
+            return name + " i cośtam...";
+        }
 	
 }
